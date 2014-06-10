f16b1213b814d7c651ccad93d63d54857caa8111
Peter Heisig <peter.heisig@medien-dresden.de>
2014-06-10 21:36:05 +0200

    small REST API adaption and service simplification

--- src/common/services/storage.js
+++ src/common/services/storage.js
@@ -2,8 +2,4 @@ angular.module('services.storage', [])
 
 .factory('suggestionsService', ['Restangular', function(Restangular) {
 	return Restangular.service('suggestions');
-}])
-
-.factory('workbenchService', ['Restangular', function(Restangular) {
-	return Restangular.service('workbench');
 }]);
