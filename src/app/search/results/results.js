b380d0a320316a3421f8aa18667d22e587e31dc3
Stefan <st3fanvogt@googlemail.com>
2014-07-27 18:25:14 +0200

    UI: results filter type

--- src/app/search/results/results.js
+++ src/app/search/results/results.js
@@ -11,6 +11,14 @@ angular.module('app.search.results', ['services.storage'])
         function ($scope, $routeParams, entityService) {
     $scope.result = [];
 
+    $scope.showDrugs = true;
+    $scope.showProteins = true;
+
+    $scope.showTypes = function(item){
+        return (item.type === 'DRUG' && $scope.showDrugs) ||
+            (item.type === 'PROTEIN' && $scope.showProteins);
+    };
+
     $scope.search = function(query, page) {
         entityService.getList({q: query, page: page}).then(function(list) {
             $scope.result = list;
