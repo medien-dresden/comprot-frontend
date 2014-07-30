797e79ded497f69c25e63485d43324132112bd72
Stefan <st3fanvogt@googlemail.com>
2014-07-30 19:04:30 +0200

    details finished. bindings missing.

--- src/app/compounds/details/details.js
+++ src/app/compounds/details/details.js
@@ -7,8 +7,23 @@ angular.module('app.compounds.details', [])
     });
 }])
 
-.controller('CompoundDetailsCtrl', ['$scope', '$routeParams',
-        function ($scope, $routeParams) {
+.controller('CompoundDetailsCtrl', ['$scope', '$routeParams', 'entityService',
+        function ($scope, $routeParams, entityService) {
     $scope.id = $routeParams.id;
+    $scope.name = "";
+    $scope.type = "";
+    $scope.sourceId = "";
+    $scope.synonyms = [];
+    $scope.targets = [];
 
+    entityService.one($scope.id).get().then(function(entity) {
+        $scope.name = entity.name;
+        $scope.type = entity.type;
+        $scope.sourceId = entity.sourceId;
+        $scope.synonyms = entity.synonyms;
+    });
+
+    entityService.one($scope.id).one('bindings').get().then(function(binding) {
+        $scope.targets = binding.content;
+    });
 }]);
--- src/app/targets/details/details.js
+++ src/app/targets/details/details.js
@@ -7,8 +7,25 @@ angular.module('app.targets.details', [])
     });
 }])
 
-.controller('TargetDetailsCtrl', ['$scope', '$routeParams',
-        function ($scope, $routeParams) {
+.controller('TargetDetailsCtrl', ['$scope', '$routeParams', 'entityService',
+        function ($scope, $routeParams, entityService) {
     $scope.id = $routeParams.id;
+    $scope.name = "";
+    $scope.type = "";
+    $scope.sourceId = "";
+    $scope.taxonomyId = "";
+    $scope.synonyms = [];
+    $scope.compounds = [];
 
+    entityService.one($scope.id).get().then(function(entity) {
+        $scope.name = entity.name;
+        $scope.type = entity.type;
+        $scope.sourceId = entity.sourceId;
+        $scope.taxonomyId = entity.taxonomyId;
+        $scope.synonyms = entity.synonyms;
+    });
+
+    entityService.one($scope.id).one('bindings').get().then(function(binding) {
+        $scope.compounds = binding.content;
+    });
 }]);
